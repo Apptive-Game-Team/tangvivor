a00e2a2845367a158e881a320767ed0492a306dd
Yunseong <dev.yunseong@gmail.com>
2025-05-13 17:17:27 +0900

    refactor to define view with xml layout files

--- app/src/main/java/com/dudoji/tangvivor/MainActivity.kt
+++ app/src/main/java/com/dudoji/tangvivor/MainActivity.kt
@@ -17,31 +17,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TangvivorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TangvivorTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
