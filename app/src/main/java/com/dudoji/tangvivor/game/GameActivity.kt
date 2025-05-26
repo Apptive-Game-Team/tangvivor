ac6f85b709683ce5faee1c408e75287070b4d169
Vackam <53655740+Vackam@users.noreply.github.com>
2025-05-26 21:37:19 +0900

    feature GameActivity : FaceDetector 적용

--- app/src/main/java/com/dudoji/tangvivor/game/GameActivity.kt
+++ app/src/main/java/com/dudoji/tangvivor/game/GameActivity.kt
@@ -1,25 +1,43 @@
 package com.dudoji.tangvivor.game
 
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.SeekBar
 import androidx.activity.ComponentActivity
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.dudoji.tangvivor.R
+import com.dudoji.tangvivor.game.camera.OnFacePositionListener
 import com.dudoji.tangvivor.game.entity.Master
 import com.dudoji.tangvivor.game.service.EnemyController
+import com.dudoji.tangvivor.game.service.FaceDetector
 import com.dudoji.tangvivor.game.service.GameLoop
 import com.dudoji.tangvivor.game.service.PlayerController
 
-class GameActivity : ComponentActivity() {
+class GameActivity : ComponentActivity(), OnFacePositionListener {
 
     lateinit var seekBar : SeekBar
     lateinit var playerController : PlayerController
     lateinit var enemyController : EnemyController
 
+    // Camera Setting
+    private lateinit var previewView: PreviewView
+    private lateinit var faceDetector: FaceDetector
     val gameLoop = GameLoop()
 
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        // Camera Setting
+        previewView = findViewById(R.id.previewView)
+        faceDetector = FaceDetector(
+            context = this,
+            lifecycleOwner = this,
+            previewView = previewView,
+            listener = this
+        )
 
         playerController = PlayerController(
             Master.User1,
@@ -60,5 +78,20 @@ class GameActivity : ComponentActivity() {
                 // Handle stop of touch
             }
         })
+    }
+
+    // Camera Function
+    override fun onFacePosition(normX: Float) {
+        playerController.setX(normX)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        faceDetector.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        faceDetector.stop()
     }
 }
