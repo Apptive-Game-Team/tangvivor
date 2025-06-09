1a99609a6a4264443aeb283fbb5ba6a7220f59e7
Vackam <53655740+Vackam@users.noreply.github.com>
2025-06-09 20:43:10 +0900

    feature(공구리 커밋) : 병합용

--- app/src/main/java/com/dudoji/tangvivor/game/entity/Session.kt
+++ app/src/main/java/com/dudoji/tangvivor/game/entity/Session.kt
@@ -3,6 +3,6 @@ package com.dudoji.tangvivor.game.entity
 import com.dudoji.tangvivor.DEFAULT_HP
 
 
-data class Session(var user1X: Float, var user2X: Float, var user1Hp: Int = DEFAULT_HP , var user2Hp: Int = DEFAULT_HP) {
-    constructor() : this(0.5f, 0.5f)
+data class Session(var user1X: Float, var user2X: Float, var user1Point: Float, var user2Point: Float, var user1Hp: Int = DEFAULT_HP , var user2Hp: Int = DEFAULT_HP, ) {
+    constructor() : this(0.5f, 0.5f, 0.5f, 0.5f)
 }
